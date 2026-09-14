ae71a38097b995717081c0a1cbcab38261fe6cf9
Aroldo Costa <aroldocosta@yahoo.com.br>
2026-09-14 00:27:17 -0300

    feat: initialize ESP32 web radio project structure and configuration files

---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char *TAG = "WEB_RADIO_MAIN";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Inicializando Web Radio ESP32...");
+}
